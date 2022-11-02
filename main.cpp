cf2dc4eaae87e15b745479cf779f62ccd39c6a08
Nicholas Cassarino <nmcassa804@outlook.com>
2022-11-02 14:53:48 -0400

    need time independent rand

--- main.cpp
+++ main.cpp
@@ -24,17 +24,61 @@ int** createBase(int row, int col) {
     return base;
 }
 
+int checkAround(int row, int col, int** arr, int cRow, int cCol) {
+    //out of 20
+    int ret = 5;
+    
+    for (int i = cRow - 1; i <= cRow + 1; i++) {
+        for (int j = cCol - 1; j <= cCol + 1; j++) {
+            if (i == -1 || j == -1 || i == row || j == col) {
+                continue;
+            } else {
+                if (arr[i][j] >=  1) {
+                    ret += 1;
+                }
+            }
+        }
+    }
+
+    return ret;
+}
+
+int** buildLayer(int row, int col, int** arr) {
+    srand (time(NULL));
+
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            int chance = checkAround(row, col, arr, i, j);
+            if (rand() % 20 < chance) {
+                arr[i][j] += 1;
+            }
+        }
+    }
+
+    return arr;
+}
+
 int main() {
     srand (time(NULL));
 
     int seed = rand();
 
-    int** table = createTable(5, 5, seed);
-    printTable(table, 5, 5);
+    //int** table = createTable(5, 5, seed);
+    //printTable(table, 5, 5);
+
+    //cout << endl;
+
+    int** base = createBase(5, 5);
+    printTable(base, 5, 5);
 
     cout << endl;
 
-    int** base = createBase(5, 5);
+    base = buildLayer(5, 5, base);
+    base = buildLayer(5, 5, base);
+    base = buildLayer(5, 5, base);
+    base = buildLayer(5, 5, base);
+    base = buildLayer(5, 5, base);
+    
     printTable(base, 5, 5);
 
     return 1;
